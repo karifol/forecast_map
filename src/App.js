2adf6dd99a1898725a763365d372733201bb8ae2
karifol <karigolhorinouchi@gmail.com>
2024-01-02 09:53:43 +0900

    dev

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import './App.css';
+import { useState, useEffect } from 'react';
+import Loading from './components/Loading/Loading';
+import Main from './components/Main/Main';
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1000);
+  }, []);
+
   return (
     <div className="App">
+      { isLoading  ? <Loading /> : <Main /> }
     </div>
   );
 }
---
+++ src/components/Main/Main.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { useState, useEffect } from 'react'
+
+function Main() {
+  return (
+    <div>Main</div>
+  )
+}
+
+export default Main
