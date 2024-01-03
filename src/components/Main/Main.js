decb73dc9af9f75c5f7bd6141fd37f5b49ece013
karifol <karigolhorinouchi@gmail.com>
2024-01-03 10:11:48 +0900

    dev

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -4,8 +4,9 @@ import Map from '../Map/Map'
 
 function Main() {
   return (
-    <div className='w-screen h-screen flex justify-center items-center'>
-      <div className='w-1/2 h-1/2'>
+    <div className='w-screen h-screen flex justify-end'>
+      <div className='bg-black w-80'>test</div>
+      <div className='w-[calc(100%_-_20rem)] h-screen'>
         <Map/>
       </div>
     </div>
